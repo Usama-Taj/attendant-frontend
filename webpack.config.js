5e9380a96ee099967bff78c9488de6b5448fefbf
usamatajj <usama.taj@hotmail.com>
2023-01-11 14:49:26 +0500

    updated build configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,44 @@
 /** @type {import('webpack').Configuration} */
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
-const DotenvWebpackPlugin = require("dotenv-webpack");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const DotenvWebpackPlugin = require('dotenv-webpack');
 const config = {
-  entry: "./src/index.tsx",
+  entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, 'docs'),
+    filename: '[name].[contenthash].js',
   },
   module: {
     rules: [
-      { test: /\.(js|ts)x?$/, exclude: /node_modules/, use: "babel-loader" },
+      { test: /\.(js|ts)x?$/, exclude: /node_modules/, use: 'babel-loader' },
 
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        type: "asset/inline",
+        type: 'asset/inline',
       },
       {
         test: /\.txt$/,
-        type: "asset/source",
+        type: 'asset/source',
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   resolve: {
-    extensions: [".js", ".json", ".jsx", ".tsx", ".ts"],
+    extensions: ['.js', '.json', '.jsx', '.tsx', '.ts'],
 
     plugins: [new TsconfigPathsPlugin()],
   },
   devServer: {
     static: {
-      directory: "./dist",
+      directory: './dist',
     },
     port: 4000,
     open: true,
@@ -49,15 +49,15 @@ const config = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      favicon: "",
+      favicon: '',
       showErrors: true,
       hash: true,
-      title: "Index HTML File Loader",
-      filename: "index.html",
-      template: "dist/index.html",
+      title: 'Index HTML File Loader',
+      filename: 'index.html',
+      template: 'dist/index.html',
     }),
     new DotenvWebpackPlugin({
-      path: "./.env",
+      path: './.env',
       safe: true,
     }),
   ],
