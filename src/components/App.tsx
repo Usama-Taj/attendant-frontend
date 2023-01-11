32702d863ec578208474fa86f1e970b12396b26a
usamatajj <usama.taj@hotmail.com>
2023-01-11 14:49:17 +0500

    remove build dir

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,7 +4,6 @@ import 'antd/dist/antd.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import MainLayout from 'layouts/EmployeeLayout/EmployeeLayout';
 import EmployeeRoutes from 'routes/EmployeeRoutes/EmployeeRoutes';
-console.log(process.env.PUBLIC_URL);
 const App: React.FC = () => {
   return (
     <Router>
