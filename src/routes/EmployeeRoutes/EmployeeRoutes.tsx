6192f9a6c5764ff12ee414acef00273eede305b5
usamatajj <usama.taj@hotmail.com>
2023-01-11 14:49:27 +0500

    updated routes

--- src/routes/EmployeeRoutes/EmployeeRoutes.tsx
+++ src/routes/EmployeeRoutes/EmployeeRoutes.tsx
@@ -1,15 +1,18 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Login from "pages/Login/Login";
-import PinConfirmation from "pages/PinConfirmation/PinConfirmation";
-import PunchCard from "pages/PunchCard/PunchCard";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from 'pages/Login/Login';
+import PinConfirmation from 'pages/PinConfirmation/PinConfirmation';
+import PunchCard from 'pages/PunchCard/PunchCard';
 
 const EmployeeRoutes: React.FC = () => {
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/pin-confirmation" element={<PinConfirmation />} />
-      <Route path="/" element={<PunchCard />} />
+      <Route path={`${process.env.PUBLIC_URL}/login`} element={<Login />} />
+      <Route
+        path={`${process.env.PUBLIC_URL}/pin-confirmation`}
+        element={<PinConfirmation />}
+      />
+      <Route path={process.env.PUBLIC_URL} element={<PunchCard />} />
     </Routes>
   );
 };
