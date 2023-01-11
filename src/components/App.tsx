b5ca2f73f64624317ea3971f97b3261d6abb0544
usamatajj <usama.taj@hotmail.com>
2023-01-11 14:49:27 +0500

    added console log

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,7 +4,7 @@ import 'antd/dist/antd.css';
 import { HashRouter as Router } from 'react-router-dom';
 import MainLayout from 'layouts/EmployeeLayout/EmployeeLayout';
 import EmployeeRoutes from 'routes/EmployeeRoutes/EmployeeRoutes';
-
+console.log(process.env.PUBLIC_URL);
 const App: React.FC = () => {
   return (
     <Router>
